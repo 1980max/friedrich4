2cdbcf73875665f4e83ef4057b2525b65cea0f66
1980max <maxvanoorde@gmail.com>
2025-04-14 20:48:58 +0200

    package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "friedrich-ai-concierge",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "start": "node api/askFriedrich.js"
+  },
+  "dependencies": {
+    "openai": "^4.20.0"
+  }
+}

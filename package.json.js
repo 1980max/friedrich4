3df7aa0efc0f1a234666dc634d0fc592a99afde6
1980max <maxvanoorde@gmail.com>
2025-04-14 20:33:54 +0200

    Add files via upload

---
+++ package.json.js
@@ -0,0 +1,14 @@
+{
+  "name": "friedrich-ai-concierge",
+  "version": "1.0.0",
+  "type": "module",
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "preview": "vite preview"
+  },
+  "devDependencies": {
+    "dotenv": "^16.3.1",
+    "vite": "^4.4.5"
+  }
+}
